dd02562b932e8a8cea7d5d43392402e92f4d1ee0
gschone-data <gillesschone@gmail.com>
2025-09-12 17:42:02 +0200

    cleaning

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import numpy as np
-from datetime import date, timedelta
-
+import datetime
 from webscrapping import load_data_all as lda
 from jinja2 import Template
 from pretty_html_table import build_table
 
-list_spots=['La-Sauzaie','Les-Dunes','Saint-Gilles-Croixde-Vie','Tanchet','La-Baie-Des-Sables','Plage-Des-Granges','Sion']
-res=lda.load_data_all(list_spots)
-#res=pd.read_csv('res_fin.csv')
+#liste des spots fixe pour le moment
+# list_spots=['La-Sauzaie','Les-Dunes','Saint-Gilles-Croixde-Vie','Tanchet','La-Baie-Des-Sables','Plage-Des-Granges','Sion','L-Aubraie','Sauveterre']
+# #recupération des données
+# res=lda.load_data_all(list_spots)
+#solution alternative tests
+res=pd.read_csv('test/res_fin.csv')
 
 #ajout lien du spot
 res.loc[:,'spot']="<a href='https://fr.surf-forecast.com/breaks/"+res['spot']+"/forecasts/latest/six_day' target='_blank'>"+res['spot']+"</a>"
 
+#consolidation une ligne par créneau
 res_fin=res.groupby(['key','date','time','rating'])['spot'].apply(lambda x: ' <br> '.join(x)).reset_index()
 
+#on enlève les ratings à 0 ou -1 (saturation)
 res_fin.loc[res_fin['rating'].isin([-1,0]), 'spot'] = ''
 #ajout icone rating
 def transform_rating(x):
@@ -25,36 +29,32 @@ def transform_rating(x):
 
 
 res_fin['rating_'] = res_fin['rating'].apply(transform_rating)
-#mise en forme date
 
+#mise en forme date
 res_fin['date'] = pd.to_datetime(res_fin['date']).dt.strftime('%d/%m/%Y')
 res_fin.sort_values(['key'], inplace=True)
+#on stock le max pour le tableau résumé
 res_fin_max=res_fin.loc[res_fin['rating'].idxmax()]
 
+#on simplifie le tableau final
 res_fin=res_fin[['date', 'time', 'rating_', 'spot']]
+
 res_fin=res_fin.rename(columns={'rating_':'Rating','spot':'Spots','time':'Quand','date':'Date'})
 
+#creation du tableau principal
 styled_df = res_fin.style.hide(axis='index')
 html_table=styled_df.to_html(index=False)       
 
-date_maj = date.today().strftime('%d/%m/%Y %H:%M')
+#creation du tableau résumé
+date_maj = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
 html_resume='<table>'
-html_resume += f'<tr><td><b>Dernière mise à jour : {date_maj}</b></td></tr>'
-html_resume += f'<tr><td><b>Meilleure session : {res_fin_max["date"]} {res_fin_max["time"]} - {res_fin_max["rating_"]} - Spots : {res_fin_max["spot"]}</b></td></tr>'
+html_resume += f'<tr><td><b>Dernière mise à jour :<b> {date_maj}</td></tr>'
+html_resume += f'<tr><td><b>Meilleure session :<b> {res_fin_max["date"]} {res_fin_max["time"]} - {res_fin_max["rating_"]} - Spots : {res_fin_max["spot"]}</td></tr>'
 html_resume += f'</table><br>'
 
 
 
-
-# Construire un tableau HTML simple
-# html_table = '<table>'
-# html_table += '<thead><tr><th>Date</th><th>Rating</th><th>Spots</th></tr></thead><tbody>'
-
-# for _, row in res_fin.iterrows():
-#     html_table += f"<tr><td>{row['date']+' '+row['time']}</td><td style='color:#a28089;'><b>{row['rating_']}</td><td>{row['spot']}</td></tr>"
-
-# html_table += '</tbody></table></body></html>'
-
+#structure de la page html
 
 template_str = """
 <!DOCTYPE html>
@@ -78,7 +78,7 @@ html_output = template.render(title="Go To Surf Vendée",
                                heading="Go To Surf Vendée")
                                
 
-with open("_site/GoToSurf.html", "w", encoding="utf8") as f:
+with open("_site/index.html", "w", encoding="utf8") as f:
     f.write(html_output)
 
 print("ok")
